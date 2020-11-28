5d07b784428d5d1de1698c885833ccbc47b8fe9b
Jacket23 <thezan234@yahoo.com>
2020-11-28 11:26:23 -0600

    Added Email Functionality

--- Load_LF4.js
+++ Load_LF4.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var app = express();
 
 //connect to our database
@@ -12,6 +13,16 @@ var connection = mysql.createPool({
   database: 'CatalinaDB_F2200'
 })
 
+//Set up mailing
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'lunchforfour@gmail.com',
+      pass: 'aurora23!'
+    }
+  });
+
+var currentDate = new Date().getDate();
 var Email = "";
 var name = "";
 
@@ -22,6 +33,12 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
+//Monthly check for participants to email
+if(currentDate == 1){
+    emailMonthly();
+}
+
+
 //Load login page
 app.get('/login', function(req, res){
     res.render( 'loginPage')
@@ -29,6 +46,104 @@ app.get('/login', function(req, res){
 
 //Authenticate the user and see if they are admin or not
 app.post('/authenticate', function(req, res){
+    authenticateUser(req, res);
+});
+
+//Adds user from sign up page
+app.post('/addUser', function(req, res){
+    addUser(req, res);
+});
+
+app.get('/loginError', function(req, res){
+    res.render('loginError')
+});
+
+app.get('/userHomePage', function(req, res){
+    res.render('userHomePage')
+});
+
+app.get('/adminHomePage', function(req, res){
+    res.render('adminHomePage')
+});
+
+app.get('/newUser', function(req, res){
+    res.render('newUser')
+});
+
+app.get('/newUserError', function(req,res){
+    res.render('newUserError')
+});
+
+app.listen(3000);
+
+//All functions go here
+//Checks for active participants and emails them at the 1st of every month
+function emailMonthly(){
+
+    connection.getConnection(function(err, connect) {
+        if (err) throw err;
+        connection.query('SELECT * FROM users', function (err, rows, fields) {
+            if (err) throw err;
+
+            var mailOptions = {
+                from: 'lunchforfour@gmail.com',
+                to: rows[user].email,
+                subject: 'Monthly Reminder from Lunch For Four',
+                text: 'We are reminding you that you have a meeting sometime this month. Please log in to check times and if you need to suspend. Thank you for using Lunch For Four!'
+              };
+
+            for(var user in rows){
+                if(rows[user].participant_status == 1){
+                    transporter.sendMail(mailOptions, function(error, info){
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log('Email sent: ' + info.response);
+                        }
+                    });
+                }
+            }
+        });
+    connection.releaseConnection(connect);
+    });
+}
+
+//Code for adding a user to the DB from the sign up page
+function addUser(req, res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var newEmail = req.body.email;
+    var password = req.body.password;
+    var ID = req.body.AUID;
+    var department = req.body.departments;
+    var AUemail = newEmail.slice(-11);
+    var AuroraEdu = "@aurora.edu";
+    var emailVerify = AuroraEdu.localeCompare(AUemail);
+    var values = [
+        [ID, firstName, lastName, newEmail, password, 0, department, 0]
+    ];
+
+    console.log(emailVerify);
+
+    if(emailVerify != 0){
+        return res.redirect('/newUserError');
+    };
+
+
+    connection.getConnection(function(err, connect) {
+        if (err) throw err;
+        connection.query(`INSERT INTO users (aurora_ID, first_name, last_name, email, password, participant_status, department, isAdmin) VALUES ?`, [values], function (err, rows, fields) {
+            if (err) throw err;
+        });
+
+
+        connection.releaseConnection(connect);
+    });
+    res.redirect('/login');
+}
+
+//Code for authenticating user
+function authenticateUser(req, res){
     var userName = req.body.email;
     var password = req.body.password;
     var notValid = true;
@@ -62,59 +177,4 @@ app.post('/authenticate', function(req, res){
         });
         connection.releaseConnection(connect);
     });
-});
-
-app.post('/addUser', function(req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var newEmail = req.body.email;
-    var password = req.body.password;
-    var ID = req.body.AUID;
-    var department = req.body.departments;
-    var AUemail = newEmail.slice(-11);
-    var AuroraEdu = "@aurora.edu";
-    var emailVerify = AuroraEdu.localeCompare(AUemail);
-    var values = [
-        [ID, firstName, lastName, newEmail, password, 0, department, 0]
-    ];
-
-    console.log(emailVerify);
-
-    if(emailVerify != 0){
-        return res.redirect('/newUserError');
-    };
-
-
-    connection.getConnection(function(err, connect) {
-        if (err) throw err;
-        connection.query(`INSERT INTO users (aurora_ID, first_name, last_name, email, password, participant_status, department, isAdmin) VALUES ?`, [values], function (err, rows, fields) {
-            if (err) throw err;
-        });
-
-
-        connection.releaseConnection(connect);
-    });
-    res.redirect('/login');
-});
-
-app.get('/loginError', function(req, res){
-    res.render('loginError')
-});
-
-app.get('/userHomePage', function(req, res){
-    res.render('userHomePage')
-});
-
-app.get('/adminHomePage', function(req, res){
-    res.render('adminHomePage')
-});
-
-app.get('/newUser', function(req, res){
-    res.render('newUser')
-});
-
-app.get('/newUserError', function(req,res){
-    res.render('newUserError')
-});
-
-app.listen(3000);
+}
